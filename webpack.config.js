1fd75206af0b1d45c73a600946213cc1a1b734a1
Gavin Crews <gavin.crews@yahoo.com>
2020-12-14 17:10:18 -0800

    boilerplate workspace

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry:['./client/index.js'],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: [".js", ".jsx", '.scss'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use:["babel-loader"]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    devServer: {
+        port: 8080,
+        open: true,
+        hot: true,
+        publicPath: "/dist/",
+        proxy: [
+            {
+              context: ["/api", "/login", "/image"],
+              target: 'http://localhost:3000',
+            },
+          ],
+
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+
+}
