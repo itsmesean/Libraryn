0fc5f095611fa3534d92de86cf44e032e52cfb82
Sean <itsmesean@pm.me>
2020-12-15 17:51:03 -0800

    Merging

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
+const router = require('./routes/routes')
 
-app.use('/', (req, res) => res.send('hello'))
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', router);
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 
 app.listen(5555, console.log('listening on port 5555'));
