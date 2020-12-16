a3866b8ca33de7fe036e3c6d8759de49d44043fa
Sean <itsmesean@pm.me>
2020-12-15 19:23:50 -0800

    Connect back to front

--- client/containers/Login.jsx
+++ client/containers/Login.jsx
@@ -4,18 +4,20 @@ import axios from 'axios';
 function Login() {
   function handleSubmit(e) {
     e.preventDefault();
-    const username = e.target[0].value;
-    const password = e.target[1].value;
+    const user = e.target[0].value;
+    const pass = e.target[1].value;
+
     const options = {
       method: 'GET',
       url: 'http://localhost:8080/api/',
-      data: { username, password },
+      params: { username: user, password: pass },
     };
     console.log(options);
     axios
       .request(options)
       .then(function (response) {
-        setBookInfo(response);
+        // setBookInfo(response);
+        console.log(response);
       })
       .catch(function (error) {
         console.error(error);
